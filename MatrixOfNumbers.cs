8bf8dbc7b5e83c3c37f3430afc267d384c69a2ad
Ivan <ivan.halkov@gmail.com>
2016-02-26 00:53:57 +0200

    Added files via upload

---
+++ MatrixOfNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class MatrixOfNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int row = 1; row <= n; row++)
+        {
+            for (int column = row; column < n + row; column++)
+            {
+                Console.Write("{0} ", column);
+            }
+            Console.WriteLine();
+        }
+    }
+}
