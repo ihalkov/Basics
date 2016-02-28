05655ede35e5d079f6ece5874d68f48f85124e10
Ivan <ivan.halkov@gmail.com>
2016-02-29 00:31:09 +0200

    Added files via upload

---
+++ RandomizeTheNumbersOneToN/RandomizeTheNumbersOneToN.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class RandomizeTheNumbersOneToN
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+        Random r = new Random();
+        for (int i = 0; i < n; i++)
+        {
+            int elem = r.Next(1, n + 1);
+            arr[i] = elem;
+            for (int j = 0; j < n; j++)
+            {
+                if (arr[i] == arr[j] && i != j)
+                {
+                    arr[i] = 0;
+                    i--;
+                }
+            }
+        }
+        foreach (var item in arr)
+        {
+            Console.Write("{0} ", item);
+        }
+    }
+}
