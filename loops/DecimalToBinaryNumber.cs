1e9e7718e436cde4507a1a4e3eff94c5d1206ca0
Ivan <ivan.halkov@gmail.com>
2016-02-29 17:49:58 +0200

    Added files via upload

---
+++ loops/DecimalToBinaryNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class DecimalToBinaryNumber
+{
+    static void Main()
+    {
+        long num = long.Parse(Console.ReadLine());  // decimal num
+        string ans = "";
+
+        do
+        {
+            ans += num % 2;
+            num /= 2;
+        }
+        while (num > 0);
+        string rightAns = "";
+        for (int i = ans.Length - 1; i >= 0; i--)
+        {
+            rightAns += ans[i];
+        }
+        Console.WriteLine(rightAns);    // answer in binary
+    }
+}
